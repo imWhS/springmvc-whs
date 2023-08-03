568213d42efe7b07897350da80afd76e7df86e55
Wonhee Son <dnjsgml1230@gmail.com>
2023-08-04 00:57:49 +0900

    init commit

---
+++ src/main/java/hello/springmvcwhs/SpringmvcWhsApplication.java
@@ -0,0 +1,13 @@
+package hello.springmvcwhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcWhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcWhsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springmvcwhs/SpringmvcWhsApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springmvcwhs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcWhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
